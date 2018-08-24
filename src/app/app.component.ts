be0fc8f5d6d29a0d87d2b369a22eabd40dce9ab0
Sami <sami.ch.27@gmail.com>
2018-08-24 22:01:01 +0100

    interactive html/css grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  data = [
+  			[null,null,null], 
+  			[null,null,null], 
+  			[null,null,null],
+  		 ];
+  player = 1;
+
+  play(line, cell) {
+  	console.log(line, cell);
+  	if (this.data[line][cell] == null) {
+	  	this.data[line][cell] = this.player;
+	  	this.player = this.player == 1 ? 0 : 1;
+	}
+  }
 }
